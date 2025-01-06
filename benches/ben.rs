62a1d110e144a128ab5ad4d3d8abcd42928cf429
Aedan Kearns <aedank@umich.edu>
2025-01-06 15:13:33 -0500

    Fix crate name in benchmark imports

--- benches/ben.rs
+++ benches/ben.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use pga3d::point::Point;
+use hydrolox_pga3d::point::Point;
 use std::hint::black_box;
 
 fn criterion_benchmark(c: &mut Criterion) {
